a7b762629d1adc2b207691ca736c947d64923f96
liuyuqi <liuyuqi.crh@gmail.com>
2014-07-14 22:49:34 -0400

    added builtin cmd: cd

--- builtin.py
+++ builtin.py
@@ -2,12 +2,15 @@
 # functions of dealing with built-in commands
 
 import utils
+import os
 
 def exec_builtin(typein):
 	'''find built-in cmds and executes it'''
 	utils.history_list[-1].cmd_found = True
 	words = typein.split(" ")
 	firstword = words[0].strip()
+	if firstword == "cd":
+		cmd_cd(words)
 	if firstword == "exit" or firstword == "quit":
 		cmd_exit()
 	if typein == "history":
@@ -26,3 +29,21 @@ def cmd_history():
 	for item in utils.history_list:
 		print(str(num)+"  "+item.cmdline)
 		num += 1
+
+def cmd_cd(words):
+	target_dir = ""
+	if len(words) == 1:
+		target_dir = "/"
+	else:
+		secword = words[1]
+		if secword == "-":
+			if utils.lwd == "":
+				print("cd: error: last dir doesn't exist!")
+				return
+			else:
+				target_dir = utils.lwd
+		else:
+			target_dir = utils.get_real_dir(secword)
+
+	os.chdir(target_dir)
+	utils.lwd = utils.cwd
--- utils.py
+++ utils.py
@@ -1,9 +1,10 @@
 # utils.py
 # definition of shell classes.
 
-builtin_cmd_list = ["exit", "history"]
+builtin_cmd_list = ["cd", "exit", "history"]
 username = ""
 cwd = ""
+lwd = ""
 pathvar = ""
 history_list = []
 
@@ -16,3 +17,31 @@ class Command (object):
 
 	def print_cmd(self):
 		print(self.cmdline)
+
+def get_real_dir(fakedir):
+	'''get the real dir name given the form starting with . or /'''
+	fullname = ""
+	if fakedir[0:2] == "..":
+		if len(fakedir) == 2:
+			fullname = cut_leaf(cwd)
+		else:
+			fullname = cut_leaf(cwd) + fakedir[2:]
+	elif fakedir[0] == ".":
+		if len(fakedir) == 1:
+			fullname = cwd
+		else:
+			fullname = cwd + fakedir[1:]
+	else:
+		fullname = cwd + "/" + fakedir
+	if fullname == "":
+		fullname = "/"
+	return fullname
+
+def cut_leaf(givendir):
+	'''returns the parent dir of a given path'''
+	leng = len(givendir)
+	for c in givendir[::-1]:
+		if c == '/' and leng != len(givendir):
+			break
+		leng -= 1
+	return givendir[:leng-1]
